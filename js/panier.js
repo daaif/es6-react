b282027e1cd6beba1be70df298fc89031a8ba011
daaif <a@daaif.net>
2020-03-11 09:17:36 +0100

    first commit

---
+++ js/panier.js
@@ -0,0 +1,7 @@
+const panier = [
+  { ref: "a", nom: "AAAA", prix: 36.00, qte: 6 },
+  { ref: "b", nom: "BBBB", prix: 20.00, qte: 3 },
+  { ref: "c", nom: "CCCC", prix: 12.5, qte: 10 },
+  { ref: "d", nom: "DDDD", prix: 60.5, qte: 6 },
+  { ref: "e", nom: "EEEE", prix: 100, qte: 8 }
+]
